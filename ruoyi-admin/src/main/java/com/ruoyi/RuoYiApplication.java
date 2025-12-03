0af5b1f44f64fe91e2e58f34ebb9b0613981c273
Zachary <woshidababa9696996@gmail.com>
2025-12-03 22:36:19 +0800

    chore: updated banners.

--- ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
+++ ruoyi-admin/src/main/java/com/ruoyi/RuoYiApplication.java
@@ -4,27 +4,27 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-/**
- * 启动程序
- * 
- * @author ruoyi
- */
-@SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
-public class RuoYiApplication
-{
-    public static void main(String[] args)
-    {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class RuoYiApplication {
+    public static void main(String[] args) {
         // System.setProperty("spring.devtools.restart.enabled", "false");
         SpringApplication.run(RuoYiApplication.class, args);
-        System.out.println("(♥◠‿◠)ﾉﾞ  若依启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
-                " .-------.       ____     __        \n" +
-                " |  _ _   \\      \\   \\   /  /    \n" +
-                " | ( ' )  |       \\  _. /  '       \n" +
-                " |(_ o _) /        _( )_ .'         \n" +
-                " | (_,_).' __  ___(_ o _)'          \n" +
-                " |  |\\ \\  |  ||   |(_,_)'         \n" +
-                " |  | \\ `'   /|   `-'  /           \n" +
-                " |  |  \\    /  \\      /           \n" +
-                " ''-'   `'-'    `-..-'              ");
+        System.out.println(
+                "   ____  ___  ____  _     ___ _   _       ____              _           _ \n" +
+                        "  / ___|/ _ \\| __ )| |   |_ _| \\ | |     | __ )  ___   ___ | |_ ___  __| |\n" +
+                        " | |  _| | | |  _ \\| |    | ||  \\| |     |  _ \\ / _ \\ / _ \\| __/ _ \\/ _` |\n" +
+                        " | |_| | |_| | |_) | |___ | || |\\  |     | |_) | (_) | (_) | ||  __/ (_| |\n" +
+                        "  \\____|\\___/|____/|_____|___|_| \\_|     |____/ \\___/ \\___/ \\__\\___|\\__,_|\n" +
+                        "                                                                          "
+        );
+
+        System.out.println(
+                "  ____                               __       _ _          _ \n" +
+                        " / ___| _   _  ___ ___ ___  ___ ___ / _|_   _| | |_   _   | |\n" +
+                        " \\___ \\| | | |/ __/ __/ _ \\/ __/ __| |_| | | | | | | | |  | |\n" +
+                        "  ___) | |_| | (_| (_|  __/\\__ \\__ \\  _| |_| | | | |_| |  |_|\n" +
+                        " |____/ \\__,_|\\___\\___\\___||___/___/_|  \\__,_|_|_|\\__, |  (_)\n" +
+                        "                                                  |___/      "
+        );
     }
 }
