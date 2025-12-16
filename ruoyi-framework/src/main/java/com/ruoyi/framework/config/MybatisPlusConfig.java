f58a914a60fab8dbbaf8c2d512f7916c484b2f04
Zachary <woshidababa9696996@gmail.com>
2025-12-16 10:40:43 +0800

    build: Integrated Mybatis-Plus to enhance Mybatis.

---
+++ ruoyi-framework/src/main/java/com/ruoyi/framework/config/MybatisPlusConfig.java
@@ -0,0 +1,38 @@
+package com.ruoyi.framework.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.BlockAttackInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement(proxyTargetClass = true)
+@Configuration
+public class MybatisPlusConfig {
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(paginationInnerInterceptor());          // pagination
+        interceptor.addInnerInterceptor(optimisticLockerInnerInterceptor());    // optimistic locker
+        interceptor.addInnerInterceptor(blockAttackInnerInterceptor());         // block attack
+        return interceptor;
+    }
+
+    public PaginationInnerInterceptor paginationInnerInterceptor() {
+        PaginationInnerInterceptor paginationInnerInterceptor = new PaginationInnerInterceptor();
+        paginationInnerInterceptor.setDbType(DbType.MYSQL);     // MySQL
+        paginationInnerInterceptor.setMaxLimit(-1L);            // Set the maximum number of items per page. The default is 500, and -1 means no limit.
+        return paginationInnerInterceptor;
+    }
+
+    public OptimisticLockerInnerInterceptor optimisticLockerInnerInterceptor() {
+        return new OptimisticLockerInnerInterceptor();
+    }
+
+    public BlockAttackInnerInterceptor blockAttackInnerInterceptor() {
+        return new BlockAttackInnerInterceptor();   // If the operation involves deleting or updating the entire table, the operation will be terminated.
+    }
+}
