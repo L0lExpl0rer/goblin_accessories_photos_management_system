8f895b85f37bd171643069528c2e28af0b5b246e
Zachary <woshidababa9696996@gmail.com>
2025-12-16 14:15:29 +0800

    build: Integrated Knife4J to enhance Swagger.

--- ruoyi-admin/src/main/java/com/ruoyi/web/core/config/SwaggerConfig.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/core/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.ruoyi.web.core.config;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -28,6 +30,7 @@ import springfox.documentation.spring.web.plugins.Docket;
  * @author ruoyi
  */
 @Configuration
+@EnableKnife4j
 public class SwaggerConfig
 {
     /** 系统基础配置 */
@@ -113,13 +116,13 @@ public class SwaggerConfig
         // 用ApiInfoBuilder进行定制
         return new ApiInfoBuilder()
                 // 设置标题
-                .title("标题：若依管理系统_接口文档")
+                .title("-- GOBLIN API Documentation --")
                 // 描述
-                .description("描述：用于管理集团旗下公司的人员信息,具体包括XXX,XXX模块...")
+                .description("General Orchestration Backend for Lazy Interactive Nerds")
                 // 作者信息
-                .contact(new Contact(ruoyiConfig.getName(), null, null))
+                .contact(new Contact("Zachary Jun", null, null))
                 // 版本
-                .version("版本号:" + ruoyiConfig.getVersion())
+                .version(ruoyiConfig.getVersion())
                 .build();
     }
 }
